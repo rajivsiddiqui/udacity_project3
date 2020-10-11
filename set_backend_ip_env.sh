0da79e853d5ac00f71b2c1c9789a3445e001b6fd
anupam-bit2004 <anupam_bit2004@hotmail.com>
2020-10-11 23:36:05 +0530

    Update set_backend_ip_env.sh

--- set_backend_ip_env.sh
+++ set_backend_ip_env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ip=$(curl -H "token: 347407f8-3d4a-4990-98c5-3b96142561b4" --request GET https://api.memstash.io/values/backend )
+ip=$(curl -H "token:100754a2-c8e8-47f6-acc3-471691e1eb0e" --request GET https://api.memstash.io/values/backend )
 echo 'testing'
 echo $ip
 export API_URL=$ip
